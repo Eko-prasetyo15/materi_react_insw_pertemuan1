9152eda0e50800366b527324a210c7d9e280a749
eko <prasetyoeko822@gmail.com>
2020-07-05 22:00:43 +0700

    component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = function () {
+    return(
+      <div>
+      <h1>ini Footer</h1>
+      <h7>created by gugun</h7>
+      </div>
+    )
+  }
+
+  export default Footer
